9ba992f6ac7882d2a2a89600ba834f8cdc1f83c1
TWG <twhiteguardian@gmail.com>
2021-12-06 13:08:58 +0100

    Update data_importer.py
    - Flake8 compliance check
    - Time series importer
    - Date filtering
    - Improved date importer
    - Better null value checking

--- Sorted Data/data_importer.py
+++ Sorted Data/data_importer.py
@@ -2,7 +2,24 @@ import struct
 from typing import List, Tuple, Optional
 
 
-def import_dates(filepath: str) -> List[Optional[Tuple[int, int, int]]]:
+def list_countries() -> List[str]:
+    """Returns a list of all countries for which data is available.
+
+    Returns:
+        List[str]: The list of country directory names.
+    """
+    output = []
+    with open('countries.txt', 'r', encoding='utf8') as file:
+        for line in file.readlines():
+            line = line.strip()
+            if len(line) < 1:
+                continue
+            output.append(line)
+
+    return output
+
+
+def import_dates(filepath: str) -> List[Tuple[int, int, int]]:
     """Reads the given file and interprets the binary data therein as a
     sequence of dates.
 
@@ -10,29 +27,35 @@ def import_dates(filepath: str) -> List[Optional[Tuple[int, int, int]]]:
         filepath (str): The path to the file to read.
 
     Returns:
-        List[Optional[Tuple[int, int, int]]]: The list of dates read from
+        List[Tuple[int, int, int]]: The list of dates read from
         the file.
     """
     output = []
     fmt = "xBHBB"
-    with open(filepath, 'rb') as file:
-        bytes = file.read()
+    # First date in dataset is February 24th, 2020
+    last_known_date = (2020, 2, 24)
+    try:
+        with open(filepath, 'rb') as file:
+            bytes = file.read()
 
-        # Each date object is 5 bytes.
-        segments = int(len(bytes) / 5)
-        for segment in range(segments):
-            block_start = segment * 5
-            block_end = block_start + 5
-            has_value, year, month, date = struct.unpack(
-                fmt,
-                b'\00' + bytes[block_start:block_end]
-            )
-            if has_value:
-                output.append((year, month, date))
-            else:
-                output.append(None)
+            # Each date object is 5 bytes.
+            segments = int(len(bytes) / 5)
+            for segment in range(segments):
+                block_start = segment * 5
+                block_end = block_start + 5
+                has_value, year, month, date = struct.unpack(
+                    fmt,
+                    b'\00' + bytes[block_start:block_end]
+                )
+                if has_value > 0:
+                    last_known_date = (year, month, date)
 
-    return output
+                output.append(last_known_date)
+
+        return output
+    except ValueError:
+        print(f'[import_dates] Invalid file path: {filepath}.')
+        return None
 
 
 def import_numerics(filepath: str) -> List[Optional[float]]:
@@ -63,7 +86,7 @@ def import_numerics(filepath: str) -> List[Optional[float]]:
                 fmt,
                 padding + bytes[block_start:block_end]
             )
-            if has_value:
+            if has_value > 0:
                 # For some reason Python makes them a factor of 10 bigger.
                 # That, or C# did it. Either way, fix this data skewing.
                 # Either Microsoft ff-ed up the double parser or there's
@@ -103,7 +126,7 @@ def import_numerics_handle_none(filepath: str, default: float) -> List[float]:
                 fmt,
                 padding + bytes[block_start:block_end]
             )
-            if has_value:
+            if has_value > 0:
                 # For some reason Python makes them a factor of 10 bigger.
                 # That, or C# did it. Either way, fix this data skewing.
                 # Either Microsoft ff-ed up the double parser or there's
@@ -118,6 +141,89 @@ def import_numerics_handle_none(filepath: str, default: float) -> List[float]:
 def import_text(filepath) -> List[str]:
     with open(filepath, 'r') as file:
         return file.readlines()
+
+
+def handle_none_time_series(data: List[Optional[float]]) -> List[float]:
+    """Removes None values from a dataset by treating it as a time series.
+    That is to say, a 'None' value means 'no change from previous observation'.
+
+    Args:
+        data (List[Optional[float]]): The dataset to remove None values from.
+
+    Returns:
+        List[float]: The resulting dataset.
+    """
+    last_data = 0.0
+    new_data = list()
+    for sample in data:
+        if sample is not None:
+            last_data = sample
+
+        new_data.append(last_data)
+
+    return new_data
+
+
+def import_time_series(filepath: str) -> List[float]:
+    """Reads a targeted file and interprets the bytes therein as a long list
+    of 64-bit floating point numbers. Empty-marked values are set to the
+    previously known value, or 0.0 for initial values.
+
+    Args:
+        filepath (str): The path to the file to read.
+
+    Returns:
+        List[float]: The time series that was read.
+    """
+    output = []
+    # Total size needs to be aligned to 16 bytes.
+    # Data is 9 bytes -> 7 bytes of padding.
+    fmt = "xxxxxxxBd"
+    padding = b'\00\00\00\00\00\00\00'
+    last_value = 0.0
+    with open(filepath, 'rb') as file:
+        bytes = file.read()
+
+        # Each data object is 9 bytes.
+        segments = int(len(bytes) / 9)
+        for segment in range(segments):
+            block_start = segment * 9
+            block_end = block_start + 9
+            has_value, value = struct.unpack(
+                fmt,
+                padding + bytes[block_start:block_end]
+            )
+            if has_value > 0:
+                # For some reason Python makes them a factor of 10 bigger.
+                # That, or C# did it. Either way, fix this data skewing.
+                # Either Microsoft ff-ed up the double parser or there's
+                # some inter-language weirdness at play.
+                last_value = value * 0.1
+
+            output.append(last_value)
+
+    return output
+
+
+def date_equal(a: Tuple[int, int, int], b: Tuple[int, int, int]) -> bool:
+    return a is not None and \
+           b is not None and \
+           a[0] == b[0] and a[1] == b[1] and a[2] == b[2]
+
+
+def limit_by_date(dates: List[Tuple[int, int, int]],
+                  start: Tuple[int, int, int],
+                  stop: Tuple[int, int, int]) -> Tuple[int, int]:
+    out_start = 0
+    out_end = len(dates) - 1
+    for i in range(out_end + 1):
+        date = dates[i]
+        if date_equal(date, start):
+            out_start = i
+        if date_equal(date, stop):
+            out_end = i
+
+    return (out_start, out_end)
 
 
 # Demo of usage.
