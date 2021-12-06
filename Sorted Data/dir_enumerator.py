6fd05197f673769bbeeb75f812144c1213e76053
TWG <twhiteguardian@gmail.com>
2021-12-06 13:13:39 +0100

    Fix over-indentation

--- Sorted Data/dir_enumerator.py
+++ Sorted Data/dir_enumerator.py
@@ -5,20 +5,20 @@ import os
 if __name__ == "__main__":
     names = []
     for x in os.walk('.'):
-            name = x[0]
-            if name == '.':
-                continue
+        name = x[0]
+        if name == '.':
+            continue
 
-            if 'Data Processor' in name:
-                continue
+        if 'Data Processor' in name:
+            continue
 
-            if '__pycache__' in name:
-                continue
+        if '__pycache__' in name:
+            continue
 
-            # We want country data, not income data.
-            if 'income' in name:
-                continue
+        # We want country data, not income data.
+        if 'income' in name:
+            continue
 
-            names.append(name[2:])
+        names.append(name[2:])
     with open('countries.txt', 'w', encoding='utf8') as file:
         file.write('\n'.join(names))
